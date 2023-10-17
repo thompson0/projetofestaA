ff5c26447fa7039748b771516b4ed06d267bc861
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-17 13:40:32 -0300

    postman, mongodb conexao

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+// registrando um usuaário
+router.post("/register", async(req, res)=>{
+const name = req.body.name;
+const email = req.body.email;
+const password = req.body.password;
+const confirmpassword = req.body.confirmpassword;
+//checando se todos os dados foram enviados
+if (name == null || email ==null || password == null || confirmpassword == null){
+return res.status(400).json({error : "Por favor, preencha todos os campos"});
+}
+//testando se as senhas são iguais
+if(password != confirmpassword){
+return res.status(400).json({error : "As senhas não conferem!!!"})
+}
+//conferindo se o usuário já existe
+const emailExists = await User.findOne({email : email});
+if(emailExists){
+return res.status(400).json({error : "O e-mail informado já existe."})
+}
+//criando a senha com bcrypt
+const salt = await bcrypt.genSalt(12);
+const passwordHash = await bcrypt.hash(password, salt);
+//criando o usuário após as validações no sistema
+const user = new User({
+name : name,
+email : email,
+password: passwordHash
+});
+//montando um try catch para pega outros erros e afins
+try {
+const newUser = await user.save();
+//criando o token do usuario
+const token = jwt.sign(
+//payload
+{
+name : newUser.name,
+id : newUser._id
+},
+"segredo" //isso tonar o nosso token único
+);
+//retornar o token para o projeto e manda mensagem
+res.json({error: null, msg: "Você fez o cadastro com sucesso!!!", token: token, userId:
+newUser._id});
+} catch(error){
+res.status(400).json({error});
+}
+});
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,38 @@
+//modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//routes
+const authRouter = require("./routes/authRoutes");
+//middlewares - travas do meio de projeto e acesso
+//config
+const dbName = "partytime";
+const port = 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+//atrelar as rotas ao express
+app.use("/api/auth", authRouter);
+
+//conexão mongodb
+mongoose.connect(
+`mongodb://127.0.0.1:27017/${dbName}`, {
+useNewUrlParser : true,
+UseUnifiedTopology : true,
+serverSelectionTimeoutMS : 10000
+}
+)
+
+app.get("/", (req, res)=> {
+//primeira rota de teste
+res.json({message : "Rota de teste será trocada!!!"});
+});
+
+//escutando a porta
+app.listen(port, ()=>{
+console.log(`O backend está rodando na porta ${port}`)
+});
